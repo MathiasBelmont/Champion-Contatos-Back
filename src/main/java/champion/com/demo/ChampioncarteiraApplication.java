c8ec94dbb85033118fa669a319848853d0fe689e
Matheus <matheusbrenner23052004@gmail.com>
2025-12-10 21:15:58 -0300

    ADD: compilação dos arquivos do springboot

---
+++ src/main/java/champion/com/demo/ChampioncarteiraApplication.java
@@ -0,0 +1,13 @@
+package champion.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampioncarteiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampioncarteiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/champion/com/demo/ChampioncarteiraApplicationTests.java
@@ -0,0 +1,13 @@
+package champion.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampioncarteiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
